18097e2786dade2566d72bea61582c7b81992173
simransyan15 <79874338+simransyan15@users.noreply.github.com>
2021-12-02 18:57:47 -0800

    Add files via upload

---
+++ app.js
@@ -0,0 +1,23 @@
+//firebase code
+// Import the functions you need from the SDKs you need
+
+// import { initializeApp } from "firebase/app";
+// import { getAnalytics } from "firebase/analytics";
+// // TODO: Add SDKs for Firebase products that you want to use
+// // https://firebase.google.com/docs/web/setup#available-libraries
+
+// // Your web app's Firebase configuration
+// // For Firebase JS SDK v7.20.0 and later, measurementId is optional
+// const firebaseConfig = {
+//   apiKey: "AIzaSyCkLMGwvt9Zd8uwbvTpQYw9yxl0TyHZjY4",
+//   authDomain: "thegoodfeetshop.firebaseapp.com",
+//   projectId: "thegoodfeetshop",
+//   storageBucket: "thegoodfeetshop.appspot.com",
+//   messagingSenderId: "1022313413423",
+//   appId: "1:1022313413423:web:71df72cf115200c816bfda",
+//   measurementId: "G-GCC6Y2QHC0"
+// };
+
+// // Initialize Firebase
+// const app = initializeApp(firebaseConfig);
+// const analytics = getAnalytics(app);
